1ca304a413a2eee3ff6736fe4adcb27cb325a4ba
rokan <rokansubhi.f@gmail.com>
2023-02-09 13:37:45 +0700

    OOP

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ require __DIR__.'/vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 $request = Request::createFromGlobals();
 $path = $request->getPathInfo();
-$route = ['/hello' => 'hello.php', '/greeting' => 'greeting.
-php'];
+$route = ['/hello' => 'hello.php', '/greeting' => 'greeting.php'];
 if (isset($route[$path])) {
 include $route[$path];
 }
