1c187c14c21c8c958df04d6643c401a867b2bcd6
rokan <rokansubhi.f@gmail.com>
2023-02-09 13:47:53 +0700

    OOP

--- greeting.php
+++ greeting.php
@@ -1,5 +1,5 @@
 <?php
 use Symfony\Component\HttpFoundation\Response;
-$response = new Response();
+
 $response->setContent(sprintf('Selamat Datang, %s', $request>get('nama')));
 
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 $request = Request::createFromGlobals();
 $path = $request->getPathInfo();
+$response = new Response();
 $route = ['/hello' => 'hello.php', '/greeting' => 'greeting.php'];
 if (isset($route[$path])) {
 include $route[$path];
