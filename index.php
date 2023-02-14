0311b08fc96d2dc9763770c2ed89da259ee695e8
rokan <rokansubhi.f@gmail.com>
2023-02-14 15:05:49 +0700

    HTTP

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
-include __DIR__ . '/config/routes.php';
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use ModernFramework\Application;
 $request = Request::createFromGlobals();
-$context = new RequestContext();
-$context->fromRequest($request);
-$matcher = new UrlMatcher($routes, $context);
-try {
-$response = new Response();
-extract($matcher->match($request->getPathInfo()));
-include sprintf('%s.php', $_route);
-} catch (ResourceNotFoundException $e) {
-$response = new Response('Halaman tidak ditemukan', Response::HTTP_NOT_FOUND);
-}
+$kernel = new Application();
+$response = $kernel->handle($request);
 $response->send();
+
