b37196f530ce2102634becc5b29ff8349677e5b0
rokan <rokansubhi.f@gmail.com>
2023-02-15 08:16:41 +0700

    HTTP

--- src/controller/HelloController.php
+++ src/controller/HelloController.php
@@ -4,5 +4,10 @@ namespace ModernFramework\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloController{
-    
+    public function hello(){
+        return new Response('Hello World');
+    }
+    public function greet($nama){
+        return Response(sprintf('Selamat Datang, %s', $nama));
+    }
 }
