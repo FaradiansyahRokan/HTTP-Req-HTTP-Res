e2720250cf27b836d2f496a5f3154091f0cbcfc2
rokan <rokansubhi.f@gmail.com>
2023-02-15 08:39:45 +0700

    HTTP

--- src/Application.php
+++ src/Application.php
@@ -7,6 +7,9 @@ use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+
 class Application implements HttpKernelInterface
 {
 public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true): Response
@@ -15,14 +18,14 @@ include __DIR__.'/../config/routes.php';
 $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
 try {
-
-$response = new Response();
-extract($matcher->match($request->getPathInfo())
-);
-include sprintf('%s/../%s.php', __DIR__, $_route
-);
-return $response;
+$request->attributes->add($matcher->match($request->getPathInfo()));
+$controller = $controllerResolver->getController
+($request);
+$arguments = $argumentResolver->getArguments($request, $controller);
+return call_user_func_array($controller, $arguments);
 } catch (ResourceNotFoundException $e) {
 return new Response('Halaman tidak ditemukan', Response::HTTP_NOT_FOUND);
 }
